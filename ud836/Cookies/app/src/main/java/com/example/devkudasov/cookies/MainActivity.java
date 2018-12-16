294a90e5f0237238563cedc0c5dd857fc0b9b7db
devkudasov <dev.kudasov@gmail.com>
2018-12-16 12:08:36 +0300

    Complete Cookies App

--- ud836/Cookies/app/src/main/java/com/example/devkudasov/cookies/MainActivity.java
+++ ud836/Cookies/app/src/main/java/com/example/devkudasov/cookies/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.devkudasov.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.devkudasov.cookies.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        TextView textView = findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
+
+        ImageView imageView = findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
     }
 }
